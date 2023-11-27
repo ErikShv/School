d37491291da64518f65543e8302d55cf173a183e
erik shvarzman <superdaimond3@gmail.com>
2023-11-27 15:14:34 +0200

    ***

---
+++ Traffic_Lights/Traffic_Lights/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Traffic_Lights.Objects;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Traffic_Lights
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private Lights _myLights;
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// הפעולה תתבצע באופן אוטומטי כאשר הדף ייפתח
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            _myLights = new Lights(elpRed, elpGreen, elpYellow);
+
+        }
+
+        private void btnManual_Click(object sender, RoutedEventArgs e)
+        {
+            _myLights.SetState();
+        }
+    }
+}
---
+++ Traffic_Lights/Traffic_Lights/Objects/Lights.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+
+namespace Traffic_Lights.Objects
+{
+    public class Lights
+    {
+        public enum TrafficLightsState   //המצאתי סוג חדש של משתנה המגדיר את מצבי הרמזור
+        {
+            red, yellow, green
+        }
+
+
+
+        private Ellipse _lightRed;
+        private Ellipse _lightGreen;
+        private Ellipse _lightYellow;
+        private bool _isAuto;         //האם הרמזור נמצא במצב אוטומטי
+
+        private TrafficLightsState _state; //הרמזור יווצר במצב אדום
+
+
+        /// <summary>
+        /// הפעולה בונה עצם רמזור
+        /// </summary>
+        /// <param name="red">עיגול אדום</param>
+        /// <param name="green">עיגול ירוק</param>
+        /// <param name="yellow">עיגול צהוב</param>
+
+        public Lights(Ellipse red, Ellipse green, Ellipse yellow)
+        {
+            _lightRed = red;
+            _lightGreen = green;
+            _lightYellow = yellow;
+            _isAuto = false;
+            _state = TrafficLightsState.red;
+        }
+
+
+        /// <summary>
+        /// הפעולה מעבירה את הרמזור ממצב הנוכחי למצב הבא באופן מעגלי
+        /// </summary>
+        public void SetState()
+        {
+            switch (_state)
+            {
+                case TrafficLightsState.red:                                 //אם הרמזור היה במצב אדום,אז 
+                    _state = TrafficLightsState.yellow;                          //עבור למצב צהוב
+                    _lightRed.Fill = new SolidColorBrush(Colors.LightGray);  //תכבה אור אדום
+                    _lightYellow.Fill = new SolidColorBrush(Colors.Yellow);  //תדליק את האור הצהוב
+                    break;
+                case TrafficLightsState.yellow:                                 //אם הרמזור היה במצב צהוב,אז 
+                    _state = TrafficLightsState.green;                          //עבור למצב ירוק
+                    _lightYellow.Fill = new SolidColorBrush(Colors.LightGray);  //תכבה אור צהוב
+                    _lightGreen.Fill = new SolidColorBrush(Colors.Green);  //תדליק את האור הירוק
+                    break;
+                case TrafficLightsState.green:                                 //אם הרמזור היה במצב ירוק,אז 
+                    _state = TrafficLightsState.red;                          //עבור למצב אדום
+                    _lightGreen.Fill = new SolidColorBrush(Colors.LightGray);  //תכבה אור ירוק
+                    _lightRed.Fill = new SolidColorBrush(Colors.Red);  //תדליק את האור האדום
+                    break;
+            }
+        }
+
+    }
+}
+
