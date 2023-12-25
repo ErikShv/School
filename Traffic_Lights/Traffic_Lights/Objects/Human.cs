7fa4fda23663e614344208af4a289c579a5f0324
erik shvarzman <superdaimond3@gmail.com>
2023-12-25 14:05:55 +0200

    ***

--- Traffic_Lights/Traffic_Lights/Objects/Human.cs
+++ Traffic_Lights/Traffic_Lights/Objects/Human.cs
@@ -3,11 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace Traffic_Lights.Objects
 {
+    
   public abstract  class Human
     {
+        public enum HumanStateType
+        {
+            idle,jumping,running
+        }
+    public Image Image { get; private set; }
+        private double _x;//מיקום האובייקט בציר אופקי
+        private double _y;//מיקום האובייקט בציר אנכי
+        private double _startx;//המיקום ההתחלתי של האובייקט בציר האופקי
+        private double _starty;//המיקום ההתחלתי של האובייקט בציר האנכי
+        protected BitmapImage _bitmapimage;
+        protected HumanStateType _humanState;//המצב
+        public Human (Image imagehuman)
+        {
+            _humanState = HumanStateType.idle;
+            Image = imagehuman;
+            _bitmapimage = new BitmapImage();
+            Image.Source = _bitmapimage;
+            MatchGifToState();
 
+
+
+        }
+        /// <summary>
+        /// הפעולה מתאימה לכל מצב העצם את הגיפ שלה
+        /// </summary>
+        /// 
+        protected virtual void MatchGifToState()
+        {
+           
+        }
+        /// <summary>
+        /// הפעולה מעבירה את מצב האדם למצב המתאים למצב הרמזור
+        /// </summary>
+        /// <param name="state"></param>
+        public void SetState(Lights.TrafficLightsState state)
+        {
+            switch (state)
+            {
+                case Lights.TrafficLightsState.red:
+                    _humanState = HumanStateType.idle;
+                    break;
+                case Lights.TrafficLightsState.yellow:
+                    _humanState = HumanStateType.jumping;
+                    break;
+                case Lights.TrafficLightsState.green:
+                    _humanState = HumanStateType.running;
+                    break;
+            }
+        }
     }
 }
