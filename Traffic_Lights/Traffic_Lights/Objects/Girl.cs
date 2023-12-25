d30326c337cfb98f0caeeb7e1ecc69394087ff4c
erik shvarzman <superdaimond3@gmail.com>
2023-12-25 14:26:26 +0200

    ***

--- Traffic_Lights/Traffic_Lights/Objects/Girl.cs
+++ Traffic_Lights/Traffic_Lights/Objects/Girl.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
 
 namespace Traffic_Lights.Objects
 {
-   public class Girl:Human
+    public class Girl : Human
     {
-    }
+        public Girl(Image imagehuman) : base(imagehuman)
+        {
+        }
+        protected override void MatchGifToState()
+        {
+            switch (_humanState)
+            {
+                case HumanStateType.idle:
+                    _bitmapimage.UriSource = new Uri("ms-appx:///Assets/Girl/girl_idle_right.gif");
+                    break;
+                case HumanStateType.jumping:
+                    _bitmapimage.UriSource = new Uri("ms-appx:///Assets/Girl/girl_walk_right.gif");
+                    break;
+                case HumanStateType.running:
+                    _bitmapimage.UriSource = new Uri("ms-appx:///Assets/Girl/girl_run_right.gif");
+                    break;
+            }
+        }
 }
