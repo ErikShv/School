3886fc081307bbe634487c52549f409df275d192
erik shvarzman <superdaimond3@gmail.com>
2023-12-25 14:22:42 +0200

    ***

--- Traffic_Lights/Traffic_Lights/Objects/Boy.cs
+++ Traffic_Lights/Traffic_Lights/Objects/Boy.cs
@@ -3,11 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
 
 namespace Traffic_Lights.Objects
 {
-   public class Boy:Human
+    public class Boy : Human
     {
-
+        public Boy(Image imagehuman) : base(imagehuman)
+        {
+          
+        }
+        protected override void MatchGifToState()
+        {
+            switch (_humanState)
+            {
+                case HumanStateType.idle:
+                    _bitmapimage.UriSource = new Uri("ms-appx:///Assets/Boy/boy_idle_right.gif");
+                    break;
+                case HumanStateType.jumping:
+                    _bitmapimage.UriSource = new Uri("ms-appx:///Assets/Boy/boy_ready_right.gif");
+                    break;
+                case HumanStateType.running:
+                    _bitmapimage.UriSource = new Uri("ms-appx:///Assets/Boy/boy_run_right.gif");
+                    break;
+            }
+        }
     }
 }
