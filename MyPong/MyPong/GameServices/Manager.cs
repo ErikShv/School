ca80b8ba8969374fd33c6f4fc60888a4784194cd
erik shvarzman <superdaimond3@gmail.com>
2024-02-19 16:01:03 +0200

    ***

---
+++ MyPong/MyPong/GameServices/Manager.cs
@@ -0,0 +1,16 @@
+﻿using MyPong.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPong.GameServices
+{
+
+    public abstract class Manager
+    {
+        //זאת רשימת כל האובייקטים שישתתפו במשחק היא תכיל את כל סוגי האובייקטים 
+        protected List<GameObject> gameObjects = new List<GameObject>();
+    }
+}
