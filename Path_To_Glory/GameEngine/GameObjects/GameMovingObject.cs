17db171f1f6b1dc2ce5264939e8a1e403ba4c54c
erik shvarzman <superdaimond3@gmail.com>
2024-10-28 15:10:39 +0200

    ***

---
+++ Path_To_Glory/GameEngine/GameObjects/GameMovingObject.cs
@@ -0,0 +1,16 @@
+﻿using GameEngine.GameServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine.GameObjects
+{
+   public abstract class GameMovingObject : GameObject
+    {
+        public GameMovingObject(Scene scene, string filename, double placeX, double placeY) : base(scene, filename, placeX, placeY)
+        {
+        }
+    }
+}
--- Path_To_Glory/GameEngine/GameServices/Scene.cs
+++ Path_To_Glory/GameEngine/GameServices/Scene.cs
@@ -1,9 +1,34 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using GameEngine.GameObjects;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.UI.Xaml.Controls;
 
 namespace GameEngine.GameServices
 {
     public abstract class Scene:Canvas
     {
+        private List<GameObject> _gameobjects = new List<GameObject>();
 
+        protected List<GameObject> _gameobjectsSnapshots => _gameobjects.ToList();
+
+        public double Ground { get; set; }  //ריצפה
+
+        public Scene()
+        {
+            Manager.GameEvent.OnRun += Run;
+            Manager.GameEvent.OnRun += CheckCollisional;
+
+        }
+
+        private void Run()
+        {
+            foreach(var GameObject in _gameobjects)
+            {
+                if(GameObject is GameMovingObject obj)
+                {
+                    obj.Render();
+                }
+            }
+        }
     }
 }
